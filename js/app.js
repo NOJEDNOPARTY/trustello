5da28a1e2124da8e945a1545f3282dfbffce7661
nojednoparty <bordyuh@gmail.com>
2024-05-21 02:18:07 +0300

    add sctructure of social field

--- js/app.js
+++ js/app.js
@@ -3,44 +3,43 @@ const common = {
 		common.preloader();
 		common.main();
 		common.splide();
+		common.socialSelect();
 		common.select();
 	},
-	preloader: () => window.addEventListener("DOMContentLoaded", () => setTimeout(() => {
+	preloader: () => window.addEventListener('DOMContentLoaded', () => setTimeout(() => {
 		document.querySelector('body').classList.add('preloaded');
 
 		new WOW().init();
 	}, 400)),
 	main: () => {
 		const menuTrigger = document.querySelectorAll('.menu-trigger');
+		const openPopupTrigger = document.querySelectorAll('.open-popup');
+		const closePopupTrigger = document.querySelectorAll('.close-popup, .popup-layout');
+		const formField = document.querySelectorAll('.form-field input, .form-field textarea');
 
-		menuTrigger.forEach(trigger => trigger.addEventListener('click', (event) => {
-			event.preventDefault();
+		menuTrigger.forEach(trigger => trigger.addEventListener('click', e => {
+			e.preventDefault();
 			document.querySelector('.header').classList.toggle('open');
 		}));
 
-		const openPopupTrigger = document.querySelectorAll('.open-popup');
-		const closePopupTrigger = document.querySelectorAll('.close-popup, .popup-layout');
-
-		openPopupTrigger.forEach(trigger => trigger.addEventListener('click', (event) => {
-			event.preventDefault();
+		openPopupTrigger.forEach(trigger => trigger.addEventListener('click', e => {
+			e.preventDefault();
 			document.querySelector('body').classList.add('popup-opened');
 		}));
 
-		closePopupTrigger.forEach(trigger => trigger.addEventListener('click', (event) => {
-			event.preventDefault();
+		closePopupTrigger.forEach(trigger => trigger.addEventListener('click', e => {
+			e.preventDefault();
 			document.querySelector('body').classList.remove('popup-opened');
 		}));
 
-		const formField = document.querySelectorAll('.form-field input, .form-field textarea');
-
 		formField.forEach(field => {
-			field.addEventListener('focusin', (event) => {
-				event.preventDefault();
+			field.addEventListener('focusin', e => {
+				e.preventDefault();
 				field.classList.add('focused');
 			});
 
-			field.addEventListener('focusout', (event) => {
-				event.preventDefault();
+			field.addEventListener('focusout', e => {
+				e.preventDefault();
 				if (!field.value)
 					field.classList.remove('focused');
 			});
@@ -65,44 +64,71 @@ const common = {
 		  	},
 		}).mount();
 	},
+	socialSelect: () => {
+		const socialSelect = document.querySelector('.social-select');
+		const selectedOption = socialSelect.querySelector('.social-select-selected');
+		const optionsList = socialSelect.querySelector('.social-select-items');
+
+		const hideOptions = () => {
+			optionsList.classList.add('social-select-hide');
+			selectedOption.classList.remove('social-select-arrow-active');
+		};
+
+		selectedOption.addEventListener('click', e => {
+			optionsList.classList.toggle('social-select-hide');
+			selectedOption.classList.toggle('social-select-arrow-active');
+		});
+
+		const options = optionsList.querySelectorAll('div');
+
+		options.forEach(option => option.addEventListener('click', () => {
+			selectedOption.innerHTML = '';
+			const clone = option.cloneNode(true);
+			selectedOption.appendChild(clone);
+			hideOptions();
+		}));
+
+		window.addEventListener('click', e =>
+			socialSelect != e.target && !socialSelect.contains(e.target) && hideOptions());
+	},
 	select: () => {
 		var x, i, j, l, ll, selElmnt, a, b, c;
-		/* Look for any elements with the class "custom-select": */
-		x = document.getElementsByClassName("selectable-element");
+		/* Look for any elements with the class 'custom-select': */
+		x = document.getElementsByClassName('selectable-element');
 		l = x.length;
 		for (i = 0; i < l; i++) {
-		  selElmnt = x[i].getElementsByTagName("select")[0];
+		  selElmnt = x[i].getElementsByTagName('select')[0];
 		  ll = selElmnt.length;
 		  /* For each element, create a new DIV that will act as the selected item: */
-		  a = document.createElement("DIV");
-		  a.setAttribute("class", "select-selected");
+		  a = document.createElement('DIV');
+		  a.setAttribute('class', 'select-selected');
 		  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
 		  x[i].appendChild(a);
 		  /* For each element, create a new DIV that will contain the option list: */
-		  b = document.createElement("DIV");
-		  b.setAttribute("class", "select-items select-hide");
+		  b = document.createElement('DIV');
+		  b.setAttribute('class', 'select-items select-hide');
 		  for (j = 1; j < ll; j++) {
 			/* For each option in the original select element,
 			create a new DIV that will act as an option item: */
-			c = document.createElement("DIV");
+			c = document.createElement('DIV');
 			c.innerHTML = selElmnt.options[j].innerHTML;
-			c.addEventListener("click", function(e) {
+			c.addEventListener('click', function(e) {
 				/* When an item is clicked, update the original select box,
 				and the selected item: */
 				var y, i, k, s, h, sl, yl;
-				s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+				s = this.parentNode.parentNode.getElementsByTagName('select')[0];
 				sl = s.length;
 				h = this.parentNode.previousSibling;
 				for (i = 0; i < sl; i++) {
 				  if (s.options[i].innerHTML == this.innerHTML) {
 					s.selectedIndex = i;
 					h.innerHTML = this.innerHTML;
-					y = this.parentNode.getElementsByClassName("same-as-selected");
+					y = this.parentNode.getElementsByClassName('same-as-selected');
 					yl = y.length;
 					for (k = 0; k < yl; k++) {
-					  y[k].removeAttribute("class");
+					  y[k].removeAttribute('class');
 					}
-					this.setAttribute("class", "same-as-selected");
+					this.setAttribute('class', 'same-as-selected');
 					break;
 				  }
 				}
@@ -111,41 +137,37 @@ const common = {
 			b.appendChild(c);
 		  }
 		  x[i].appendChild(b);
-		  a.addEventListener("click", function(e) {
-			/* When the select box is clicked, close any other select boxes,
-			and open/close the current select box: */
+		  a.addEventListener('click', function(e) {
 			e.stopPropagation();
 			closeAllSelect(this);
-			this.nextSibling.classList.toggle("select-hide");
-			this.classList.toggle("select-arrow-active");
+			this.nextSibling.classList.toggle('select-hide');
+			this.classList.toggle('select-arrow-active');
 		  });
 		}
 
 		function closeAllSelect(elmnt) {
-		  /* A function that will close all select boxes in the document,
-		  except the current select box: */
 		  var x, y, i, xl, yl, arrNo = [];
-		  x = document.getElementsByClassName("select-items");
-		  y = document.getElementsByClassName("select-selected");
+		  x = document.getElementsByClassName('select-items');
+		  y = document.getElementsByClassName('select-selected');
 		  xl = x.length;
 		  yl = y.length;
 		  for (i = 0; i < yl; i++) {
 			if (elmnt == y[i]) {
 			  arrNo.push(i)
 			} else {
-			  y[i].classList.remove("select-arrow-active");
+			  y[i].classList.remove('select-arrow-active');
 			}
 		  }
 		  for (i = 0; i < xl; i++) {
 			if (arrNo.indexOf(i)) {
-			  x[i].classList.add("select-hide");
+			  x[i].classList.add('select-hide');
 			}
 		  }
 		}
 
 		/* If the user clicks anywhere outside the select box,
 		then close all select boxes: */
-		document.addEventListener("click", closeAllSelect);
+		document.addEventListener('click', closeAllSelect);
 	},
 };
 
